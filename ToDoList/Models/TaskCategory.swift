e8b0fbdf71b04fdac9b8d2e63db20d3ab03ba1ac
Murilo Scheffel <murilo.scheffel@outlook.com>
2025-11-18 10:10:47 -0300

    setup data models

---
+++ ToDoList/Models/TaskCategory.swift
@@ -0,0 +1,8 @@
+//
+//  TaskCategory.swift
+//  ToDoList
+//
+//  Created by aluno-15 on 18/11/25.
+//
+
+import Foundation
---
+++ ToDoList/Screens/Models/Task.swift
@@ -0,0 +1,8 @@
+//
+//  Task.swift
+//  ToDoList
+//
+//  Created by aluno-15 on 18/11/25.
+//
+
+import Foundation
