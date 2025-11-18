592393db43fcfa525675f1263e85ac8349fddbcd
Murilo Scheffel <murilo.scheffel@outlook.com>
2025-11-18 10:55:55 -0300

    TaskView created

---
+++ ToDoList/Components/TaskView.swift
@@ -0,0 +1,34 @@
+//
+//  TaskView.swift
+//  ToDoList
+//
+//  Created by aluno-15 on 18/11/25.
+//
+
+import SwiftUI
+
+struct TaskView: View {
+    
+    var task: Task
+    
+    var body: some View {
+        HStack(alignment: .center, spacing: 16) {
+            Button {
+//                task.isCompleted.toggle()
+            } label: {
+                Image(systemName: task.isCompleted ? "checkmark.circle.fill" : "checkmark.circle")
+                    .foregroundStyle(task.isCompleted ? .accent : .gray3)
+                    .frame(width: 22, height: 22)
+            }
+            Text(task.name)
+                .foregroundStyle(.labelPrimary)
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.vertical, 11)
+        }
+        .padding(.leading, 11)
+    }
+}
+
+#Preview {
+    TaskView(task: Task(name: "Almoçar", details: "no RU", category: .groceries, isCompleted: true))
+}
--- ToDoList/Screens/Tasks.swift
+++ ToDoList/Screens/Tasks.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct Tasks: View {
     
     var tasks: [Task] = [
+        Task(name: "Tomar café da manhã", details: "no RU", category: .groceries, isCompleted: false),
         Task(name: "Almoçar", details: "no RU", category: .groceries, isCompleted: false),
         Task(name: "Jantar", details: "em casa", category: .groceries, isCompleted: false)
     ]
@@ -21,7 +22,8 @@ struct Tasks: View {
         } else {
             
             List(tasks) { task in
-                Text(task.name)
+                TaskView(task: task)
+                    .listRowInsets(EdgeInsets())
             } .listStyle(.plain)
         }
         
